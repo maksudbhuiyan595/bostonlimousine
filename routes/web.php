169404ef1dbd049b0f2b23e0fa42c8707d16109e
maksudbhuiyan595 <mdmaksudbhuiyan595@gmail.com>
2026-04-22 00:39:58 +0600

    landing page initail

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
+use App\Http\Controllers\AppController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RouteController;
 use App\Models\BlogPost;
 use App\Models\City;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
 
-    return view('welcome');
+Route::controller(AppController::class)->group(function () {
+    Route::get('/', 'home');
+
 });
