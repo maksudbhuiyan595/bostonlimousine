a7c2a70226dc2b5d83fa2a46bb365020c65219dc
maksudbhuiyan595 <mdmaksudbhuiyan595@gmail.com>
2026-06-07 23:55:32 +0600

    text replace and nav color chnage

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,7 +3,7 @@
 
     <head>
         <meta charset="utf-8">
-        <title>Boston Airport Transfer</title>
+        <title>Logan Airport Transfer</title>
         <meta content="width=device-width, initial-scale=1.0" name="viewport">
         <meta content="" name="keywords">
         <meta content="" name="description">
